58e7d0ea43408680611d63854a80adb2a16538b7
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-06 21:47:00 +0700

    learn switch case

---
+++ SwitchCase.Demo/SwitchCase.Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿int price = 10;
+int price2 = 15;
+
+// Syntax normal
+switch((price, price2))
+{
+    case ( <= 5, < 5):
+        Console.WriteLine("Buy");
+        break;
+    case ( > 5, >= 10):
+        Console.WriteLine("Bergain");
+        break;
+    default:
+        Console.WriteLine("Do not buy");
+        break;
+}
+
+// Syntax Short
+var result = (price, price2) switch
+{
+    ( <= 5, < 5) => "Buy",
+    ( > 5, >= 10) => "Bergain",
+    _ => "Do not buy"
+};
+Console.WriteLine(result);
+Console.ReadLine();
