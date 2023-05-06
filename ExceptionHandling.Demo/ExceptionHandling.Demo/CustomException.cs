f347dd4ac85485ca4d275b79eee07246591333f9
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-06 21:07:59 +0700

    learn exception handling

---
+++ ExceptionHandling.Demo/ExceptionHandling.Demo/CustomException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Demo
+{
+    public class CustomException : Exception
+    {
+        public int Count { get; set; }
+        public CustomException() : base()
+        {
+        }
+
+        public CustomException(string? message) : base(message)
+        {
+        }
+
+        public CustomException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ ExceptionHandling.Demo/ExceptionHandling.Demo/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ExceptionHandling.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Test(@"C:\Test.txt");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex}");
+            }
+        }
+
+        private static void Test(string fileName)
+        {
+            if(string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("File name should be not null or empty");
+            }
+            try
+            {
+                if(!File.Exists(fileName))
+                {
+                    var ex = new CustomException("Error occured");
+                    ex.Data.Add("Key", "Value");
+                    throw ex;
+                }
+                File.OpenRead(fileName);
+                Console.WriteLine("Hello world");
+            }
+            catch (CustomException ex) when (ex.Data.Contains("Key"))
+            {
+                Console.WriteLine($"Error: {ex}, Count: {ex.Count}");
+                throw;
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
