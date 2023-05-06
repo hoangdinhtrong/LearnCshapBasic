fbbd4d9df41184dea8829ec0d85d8f719440b01a
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-06 20:15:09 +0700

    learn about delegate and event

---
+++ DelegateEvent.Demo/DelegateEvent.Demo/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEvent.Demo
+{
+    public class Calculator
+    {
+        #region Delegate and Func
+        //public delegate int Calculate(int input);
+
+        //public int Execute(Calculate calculate, int input)
+        //{
+        //    return calculate(input);
+        //}
+
+        //public int Execute(Func<int, int> calculate, int input)
+        //{
+        //    return calculate(input);
+        //}
+        #endregion
+
+        #region Delegate and Action
+        //public delegate void Print(int input);
+        //public int Execute(Func<int, int> calculate, Print print, int input)
+        //{
+        //    var result = calculate(input);
+        //    print(result);
+        //    return result;
+        //}
+
+        //public int Execute(Func<int, int> calulate, Action<int> print, int input)
+        //{
+        //    var result = calulate(input);
+        //    print(result);
+        //    return result;
+        //}
+        #endregion
+
+        #region Delegate and Event
+        public event Action<object, CalculatorEventArgs>? Calculate;
+
+        public void RaiseEvent(string name)
+        {
+            Calculate?.Invoke(this, new CalculatorEventArgs() { Name = name });
+        }
+
+        public int Execute(Func<int, int> calulate, Action<int> print, int input)
+        {
+            var result = calulate(input);
+            print(result);
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ DelegateEvent.Demo/DelegateEvent.Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DelegateEvent.Demo
+{
+    internal class Program
+    {
+        static int Square(int input) => input * input;
+        static void Main(string[] args)
+        {
+            var caltor = new Calculator();
+            //Calculator.Calculate calc = Square;
+
+            //var respomnse = caltor.Execute(Square, Console.WriteLine,5);
+
+            //Use lambda
+            //var respomnse = caltor.Execute((x) => x * x, Console.WriteLine, 5);
+
+            //caltor.Calculate += Caltor_Calculate;
+            caltor.Calculate += (obj, arg) => Console.WriteLine(arg.Name);
+            caltor.RaiseEvent("Test name");
+            //Console.WriteLine(respomnse);
+            Console.ReadKey();
+        }
+
+        private static void Caltor_Calculate(object arg1, CalculatorEventArgs arg2)
+        {
+            Console.WriteLine(arg2.Name);
+        }
+    }
+}
