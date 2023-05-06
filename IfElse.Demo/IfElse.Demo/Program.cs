7b32f058985f43393bf25b1cd10f30bafe73485d
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-06 21:29:16 +0700

    Lear if else condition

---
+++ IfElse.Demo/IfElse.Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿int price = 10;
+if(price <= 5)
+{
+    Console.WriteLine("Buy");
+}
+else if(price > 5 && price <= 10)
+{
+    Console.WriteLine("Bergain");
+}
+else if(price > 10 && price <= 15)
+{
+    Console.WriteLine("Think");
+}
+else
+{
+    Console.WriteLine("Do not buy");
+}
+
+Console.ReadLine();
