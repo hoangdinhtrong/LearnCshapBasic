937e9ee03fd3b1e0df6561c92517d34f6e93f467
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-07 22:24:37 +0700

    learn attribute

---
+++ Attribute.Demo/Attribute.Demo/AttributeExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attribute.Demo
+{
+    [Serializable]
+    [Test("Attribute test", Quantity = 10)]
+    public class AttributeExample
+    {
+    }
+
+    public class TestAttribute: System.Attribute
+    {
+        private readonly string name;
+        public int Quantity;
+
+        public TestAttribute(string name) { this.name = name; }
+
+        public string Name => name;
+    }
+}
---
+++ Attribute.Demo/Attribute.Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using Attribute.Demo;
+
+var attributes = typeof(AttributeExample).GetCustomAttributes(true);
+foreach(var att in attributes)
+{
+    if(att is TestAttribute)
+    {
+        var attValue = (TestAttribute)att;
+        Console.WriteLine($"Name: {attValue.Name}, Quantity: {attValue.Quantity}");
+    }
+}
+
+Console.ReadLine();
