ef77de2ba5aed617c9a59647b0cce1a260b0158f
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-08 22:18:16 +0700

    learn about where in linq

---
+++ LinqInternals.Demo/LinqInternals.Demo/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace LinqInternals.Demo.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<T> NewWhere<T>(this IEnumerable<T> items, 
+            Func<T, bool> predicate)
+        {
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ LinqInternals.Demo/LinqInternals.Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using LinqInternals.Demo.Extensions;
+
+namespace LinqInternals.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleWhereLinq();
+            Console.ReadLine();
+        }
+
+        private static void ExampleWhereLinq()
+        {
+            var items = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var eventItems = items.NewWhere(x => x % 2 == 0);
+            foreach (var item in eventItems)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
