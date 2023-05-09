cc465717a3756a5cae006e182f0e0ffa20e8b67c
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-09 22:08:03 +0700

    learn a bit of pattern matching

---
+++ PatternMatching.Demo/PatternMatching.Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PatternMatching.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person() { Age = 25};
+
+            Console.WriteLine($"Contribution for person: {Contribution(person)}");
+            Console.WriteLine($"isTeacher: {IsTeacher(new Teacher())}");
+
+            Console.ReadLine();
+        }
+
+        static double Contribution(Person person) => person.Age switch
+        {
+            > 50 or >= 50 => 25500,
+            < 50 and > 18 => 19500,
+            _ => 0
+        };
+
+        static bool IsTeacher(Person person) => person switch
+        {
+            not Teacher => false,
+            not Student => true,
+        };
+    }
+
+    class Person
+    {
+        public int Age { get; set; }
+    }
+
+    class Teacher : Person { }
+    class Student: Person { }
+}
