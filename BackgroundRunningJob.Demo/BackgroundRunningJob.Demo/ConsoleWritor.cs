76fd930ded187b02769c6355613307be75ef0742
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-14 22:42:28 +0700

    learn about running background job

---
+++ BackgroundRunningJob.Demo/BackgroundRunningJob.Demo/ConsoleWritor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+
+public class ConsoleWritor : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Console.WriteLine("Running!");
+            await Task.Delay(3000);
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
