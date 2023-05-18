a864ed28de66b986c81ef7321dbde37ebc39c308
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-18 21:29:30 +0700

    learn about Parallet

---
+++ Cshap7.Demo/Parrallel.Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// For Loop
+Parallel.For(0, 10, count => Console.WriteLine(count));
+
+// Foreach Loop
+var items = new[] { "one", "two", "three", "four" };
+await Parallel.ForEachAsync(items, async (x, token) => Console.WriteLine(await GetNameAsync(x)));
+
+async Task<string> GetNameAsync(string name)
+{
+    return await Task.FromResult($"{name}{Random.Shared.Next(10)}");
+}
+
+// Invoke
+Parallel.Invoke(() => Console.WriteLine("First"),
+    () => Console.WriteLine("Secound"),
+    () => Console.WriteLine("Third"),
+    () => Console.WriteLine("Fourth")
+);
+
+Console.ReadLine();
