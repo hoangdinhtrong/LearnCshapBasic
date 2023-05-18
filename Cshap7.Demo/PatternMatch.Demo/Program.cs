83adee6594e0b346dd4db15e4ba603f6088a13ed
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-05-18 21:47:16 +0700

    learn pattern matching in net7

---
+++ Cshap7.Demo/PatternMatch.Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using PatternMatch.Demo;
+
+User user = new User() { Name = "John Doe", Age = 20};
+
+if(user is User { Name: { Length: > 5 } })
+{
+    Console.WriteLine("Name Matched");
+}
+
+if(user is User { Age: > 10 })
+{
+    Console.WriteLine("Age Matched");
+}
+
+if(user.Name is string && (user.Name == "John Doe" | user.Name == "Jane Doe"))
+{
+    Console.WriteLine("Matched");
+}
+
+Console.WriteLine(findMatch(user.Name));
+
+string findMatch(object value) => value switch
+{
+    int i when i > 0 => "+ve",
+    int i => "-ve",
+    string str when str.Length < 5 => "short",
+    string str => "long",
+    _ => "N/A"
+};
+
+Console.ReadLine();
